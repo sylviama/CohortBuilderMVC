bc102682e8b0b4e76f8541b056d7e54cfc74c20a
Kate <katerebekah@gmail.com>
2016-10-12 17:35:04 -0500

    preparing project

---
+++ CohortBuilderMVC/DAL/Models/Cohort.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControllerAndRazorReview.DAL.Models
+{
+    public class Cohort
+    {
+        public string Name { get; set; }
+        public bool FullTime { get; set; }
+        public bool Active { get; set; }
+        public List<Student> Students { get; set; }
+        public Instructor PrimaryInstructor { get; set; }
+        public List<Instructor> JuniorInstructors { get; set; }
+    }
+}
---
+++ CohortBuilderMVC/DAL/Models/Instructor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControllerAndRazorReview.DAL.Models
+{
+    public class Instructor
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+        public bool Active { get; set; }
+    }
+}
