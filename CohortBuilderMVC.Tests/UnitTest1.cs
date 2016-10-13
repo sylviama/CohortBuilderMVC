af3869484424cc4e68d9853685401eb8c704ccfa
Sylvia Ma <xiangwma@umich.edu>
2016-10-12 19:29:28 -0500

    unit test part is OK

---
+++ CohortBuilderMVC.Tests/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using CohortBuilderMVC.Controllers;
+using Moq;
+using ControllerAndRazorReview.DAL;
+using ControllerAndRazorReview.DAL.Models;
+using System.Collections.Generic;
+
+namespace CohortBuilderMVC.Tests
+{
+    [TestClass]
+    public class CohortControllerTest
+    {
+        private Mock<CohortBuilder> mockCohortBuilder;
+        private List<Cohort> cohorts;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            mockCohortBuilder = new Mock<CohortBuilder>();
+            cohorts = new List<Cohort>();
+            mockCohortBuilder.Setup(x => x.GetAllCohorts()).Returns(cohorts);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            mockCohortBuilder = null;
+            cohorts = null;
+        }
+
+        [TestMethod]
+        public void CanMakeInstanceOfCohortController()
+        {
+
+        }
+        [TestMethod]
+        public void canMakeInstanceOfCohortController_withCohortBuilder()
+        {
+            CohortController controller = new CohortController();
+            Assert.IsNotNull(controller);
+        }
+
+        [TestMethod]
+        public void CohortControllerCouldReturnView()
+        {
+            CohortController controller = new CohortController();
+            ViewResult result = controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void CohortControllerCanReturnAllCohortsInIndex()
+        {
+            CohortController controller = new CohortController(mockCohortBuilder.Object);
+            ViewResult result = controller.Index() as ViewResult;
+            var actualCohorts = result.ViewBag.Cohorts;
+
+            CollectionAssert.AreEqual(actualCohorts, cohorts);
+        }
+    }
+}
--- CohortBuilderMVC/Controllers/CohortController.cs
+++ CohortBuilderMVC/Controllers/CohortController.cs
@@ -1,30 +1,32 @@
-﻿using ControllerAndRazorReview.DAL;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CohortBuilderMVC.Controllers;
+using ControllerAndRazorReview.DAL;
+
 
 namespace CohortBuilderMVC.Controllers
 {
     public class CohortController : Controller
     {
-        private CohortBuilder cohortBuilder { get; set; }
+        private CohortBuilder _cohort { get; set; }
 
         public CohortController()
         {
-            cohortBuilder = new CohortBuilder();
+            _cohort = new CohortController();
         }
 
-        public CohortController(CohortBuilder builder)
+        public CohortController(CohortBuilder cohort)
         {
-            cohortBuilder = builder;
+            _cohort = cohort;
         }
-        
+            
         // GET: Cohort
         public ActionResult Index()
         {
-            ViewBag.Cohorts = cohortBuilder.GetAllCohorts();
+            ViewBag.cohorts = _cohort.GetAllCohorts();
             return View();
         }
     }
